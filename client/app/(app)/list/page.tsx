7a003acd09a60698d4251d84f15682a01865aca9
Mattia23Dev <mattianoris23@gmail.com>
2024-06-22 15:53:51 +0200

    quasi fatto il caricamento pdf

--- client/app/(app)/list/page.tsx
+++ client/app/(app)/list/page.tsx
@@ -1,69 +1,219 @@
 'use client';
 
-import React, { useState } from 'react';
-import Link from 'next/link';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { FcIdea } from 'react-icons/fc';
+import Tesseract from 'tesseract.js';
 import { MdAddCircleOutline, MdTextFields, MdPhotoCamera, MdFileUpload } from 'react-icons/md';
 import AuthWrapper from '@/components/Providers/AuthWrapper';
 import TextCardPopup from '@/components/Layout/AppLayout/TextCardPopup';
 import QrCodePopup from '@/components/Layout/AppLayout/QrCodePopup';
+import axios from 'axios';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area';
+import { Cross2Icon, EyeOpenIcon, DownloadIcon } from '@radix-ui/react-icons';
+import { Document, Page, pdfjs } from 'react-pdf';
+import FilePopup from "@/components/Layout/AppLayout/FilePopup";
 
-const ListPage: React.FC = () => {
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+
+  const ListPage: React.FC = () => {
     const [isPopupOpenText, setIsPopupOpenText] = useState(false);
     const [isPopupOpenQr, setIsPopupOpenQr] = useState(false);
+    const [isPopupOpenPdf, setIsPopupOpenPdf] = useState(false);
     const [file, setFile] = useState<File | null>(null);
+    const [images, setImages] = useState<string[]>([]);
     const [activeMap, setActiveMap] = useState(true);
     const [activeFolder, setActiveFolder] = useState(false);
-
-    const handleOpenPopup = (open: String) => {
-        if (open === "zero"){
-            console.log(open)
-        } else if (open === "text"){
-            setIsPopupOpenText(true);
-        } else if (open === "qr"){
-            setIsPopupOpenQr(true);
-        } else {
-            console.log(open)
-        }
+    const [selectedChapter, setSelectedChapter] = useState<string[]>([]);
+    const [chapters, setChapters] = useState<{ [key: string]: string[] }>({});
+    const [extractedTexts, setExtractedTexts] = useState<{ [key: string]: string }>({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [numPages, setNumPages] = useState<number | null>(null);
+    const [imageUrlArray, setImageUrlArray] = useState<string[] | null>(null);
+    const [fileType, setFileType] = useState<string | null>(null);
+    const [selectedPDFFile, setSelectedPDFFile] = useState<File | null>(null);
+  
+    const handleOpenPopup = (open: string) => {
+      if (open === "zero") {
+        console.log(open);
+      } else if (open === "text") {
+        setIsPopupOpenText(true);
+      } else if (open === "qr") {
+        setIsPopupOpenQr(true);
+      } else if (open === "file"){
+        setIsPopupOpenPdf(true);
+      } else {
+        console.log(open);
+      }
     };
   
     const handleClosePopup = () => {
       setIsPopupOpenText(false);
       setIsPopupOpenQr(false);
+      setIsPopupOpenPdf(false);
     };
-
-	const handleUploadFile = (event: React.ChangeEvent<HTMLInputElement>) => {
-	  const file = event.target.files ? event.target.files[0] : null;
-	  setFile(file);
-	};
-
-  return (
-    <AuthWrapper>
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-8">Crea!</h1>
-      <div className="flex justify-center space-x-4 mb-8">
-        <Button onClick={() => handleOpenPopup("zero")} icon={<MdAddCircleOutline size={24} />} text="Crea da zero" />
-        <Button onClick={() => handleOpenPopup("text")} icon={<MdTextFields size={24} />} text="Crea da Testo" />
-        <Button onClick={() => handleOpenPopup("qr")} icon={<MdPhotoCamera size={24} />} text="Crea da Foto" />
-        <StyledFileInput onChange={handleUploadFile} />
-        <Button onClick={() => handleOpenPopup("ideas")} icon={<FcIdea size={24} />} text="Genera idee" />
-      </div>
-      <div className="border-b mb-4">
-        <nav className="flex space-x-4">
-          <Tab onClick={() => {setActiveMap(true); setActiveFolder(false)}} text="Materiale" active={activeMap} />
-          <Tab onClick={() => {setActiveMap(false); setActiveFolder(true)}} text="Cartelle" active={activeFolder} />
-        </nav>
-      </div>
-      <div className="text-center text-gray-600">
-        Qui troverai tutte le mappe che hai creato. Crea una mappa per iniziare!
-      </div>
-    </div>
-    <TextCardPopup isOpen={isPopupOpenText} onClose={handleClosePopup} />
-    <QrCodePopup isOpen={isPopupOpenQr} onClose={handleClosePopup} />
-    </AuthWrapper>
-  );
-};
-
+  
+    const handleImage = useCallback(
+      (event: React.ChangeEvent<HTMLInputElement>) => {
+        setImageUrlArray([]);
+        const file = event.target.files?.[0];
+  
+        if (file && file.type === 'application/pdf') {
+          setIsLoading(true);
+          setSelectedPDFFile(file);
+        } else if (file) {
+          setFileType('image');
+          setImageUrlArray([URL.createObjectURL(file)]);
+        }
+      },
+      []
+    );
+  
+    const onLoadSuccess = useCallback(
+      ({ numPages }: { numPages: number }) => {
+        setNumPages(numPages);
+        setIsLoading(false);
+      },
+      []
+    );
+  
+    const onRenderSuccess = useCallback(
+      (pageIndex: number) => {
+        if (numPages) {
+          Array.from(new Array(numPages), (el, index) => {
+            const importPDFCanvas = document.querySelector<HTMLCanvasElement>(
+              `.import-pdf-page-${index + 1} canvas`
+            );
+  
+            if (pageIndex === index && importPDFCanvas) {
+              importPDFCanvas.toBlob((blob) => {
+                if (blob) {
+                  setImageUrlArray((prev) => [
+                    ...(prev || []),
+                    URL.createObjectURL(blob),
+                  ]);
+                }
+              });
+            }
+          });
+        }
+      },
+      [numPages]
+    );
+  
+    const uploadImagesToWordPress = async (images: string[]) => {
+      const uploadedImages: string[] = [];
+      for (const image of images) {
+        const response = await uploadImage(image);
+        if (response) {
+          uploadedImages.push(response);
+        }
+      }
+      return uploadedImages;
+    };
+  
+    const uploadImage = async (image: string) => {
+      try {
+        const formData = new FormData();
+        const blob = dataURItoBlob(image);
+        formData.append('file', blob, 'image.jpg');
+  
+        const response = await axios.post('https://studdyedu.com/wp-json/wp/v2/media', formData, {
+          headers: {
+            'Content-Disposition': 'attachment; filename="image.jpg"',
+            'Content-Type': 'multipart/form-data',
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_WTOKEN}`, // Sostituisci con il token di autenticazione WordPress
+          },
+        });
+  
+        return response.data.source_url;
+      } catch (error) {
+        console.error('Error uploading image:', error);
+        return null;
+      }
+    };
+  
+    const dataURItoBlob = (dataURI: string) => {
+      const byteString = atob(dataURI.split(',')[1]);
+      const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+      const ab = new ArrayBuffer(byteString.length);
+      const ia = new Uint8Array(ab);
+      for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+      return new Blob([ab], { type: mimeString });
+    };
+  
+    return (
+      <AuthWrapper>
+        <div className="min-h-screen bg-gray-100 p-6">
+          <h1 className="text-3xl font-bold text-center mb-8">Crea!</h1>
+          <div className="flex justify-center space-x-4 mb-8">
+            <Button onClick={() => handleOpenPopup("zero")} icon={<MdAddCircleOutline size={24} />} text="Crea da zero" />
+            <Button onClick={() => handleOpenPopup("text")} icon={<MdTextFields size={24} />} text="Crea da Testo" />
+            <Button onClick={() => handleOpenPopup("qr")} icon={<MdPhotoCamera size={24} />} text="Crea da Foto" />
+            <Button onClick={() => handleOpenPopup("file")} icon={<MdPhotoCamera size={24} />} text="Crea da File" />
+            <Button onClick={() => handleOpenPopup("ideas")} icon={<FcIdea size={24} />} text="Genera idee" />
+          </div>
+          <div className="border-b mb-4">
+            <nav className="flex space-x-4">
+              <Tab onClick={() => { setActiveMap(true); setActiveFolder(false); }} text="Materiale" active={activeMap} />
+              <Tab onClick={() => { setActiveMap(false); setActiveFolder(true); }} text="Cartelle" active={activeFolder} />
+            </nav>
+          </div>
+          <div className="text-center text-gray-600">
+            Qui troverai tutte le mappe che hai creato. Crea una mappa per iniziare!
+          </div>
+        </div>
+        <TextCardPopup isOpen={isPopupOpenText} onClose={handleClosePopup} />
+        <QrCodePopup isOpen={isPopupOpenQr} onClose={handleClosePopup} />
+        <FilePopup isOpen={isPopupOpenPdf} onClose={handleClosePopup} />
+        {isLoading && <div />}
+  
+        {selectedPDFFile && (
+          <div>
+            <Document
+              file={selectedPDFFile}
+              onLoadSuccess={onLoadSuccess}
+              error={<div>An error occurred!</div>}
+            >
+              {Array.from(new Array(numPages), (el, index) => (
+                <React.Fragment key={`page_${index + 1}`}>
+                  <Page
+                    pageNumber={index + 1}
+                    className={`import-pdf-page-${index + 1} ${fileType === 'image'}`}
+                    onRenderSuccess={() => onRenderSuccess(index)}
+                    width={1024}
+                    renderTextLayer={false}
+                    renderAnnotationLayer={false}
+                    error={<div>An error occurred!</div>}
+                  />
+                  {imageUrlArray && imageUrlArray[index] && (
+                    <a href={imageUrlArray[index]} download>
+                      download file
+                    </a>
+                  )}
+                </React.Fragment>
+              ))}
+            </Document>
+          </div>
+        )}
+  
+        {!!imageUrlArray?.length &&
+          fileType === 'image' &&
+          imageUrlArray.map((image: string, index: number) => (
+            <div key={`page_${index + 1}`}>
+              <img src={image} />
+              <a href={image} download>
+                download file
+              </a>
+            </div>
+          ))}
+      </AuthWrapper>
+    );
+  };
+  
 interface StyledFileInputProps {
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
@@ -104,5 +254,6 @@ const Tab: React.FC<{ text: string; active?: boolean; onClick: () => void }> = (
     </button>
   );
 };
+
 
 export default ListPage;
---
+++ client/components/Layout/AppLayout/FilePopup.tsx
@@ -0,0 +1,266 @@
+import React from 'react'
+import { useCallback, useState } from 'react';
+import { MdAddCircleOutline, MdFileUpload } from 'react-icons/md';
+import { Document, Page, pdfjs } from 'react-pdf';
+import '../../../app/page.module.css'
+
+interface FilePopupProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+interface ImagePopupProps {
+  isOpen: boolean;
+  images: string[];
+  onClose: () => void;
+}
+
+interface ChapterImages {
+  [key: string]: string[];
+}
+
+const ImagePopup: React.FC<ImagePopupProps> = ({ isOpen, images, onClose }) => {
+  const [chapters, setChapters] = useState<ChapterImages>({ 'Capitolo 1': [] });
+  const [activeChapter, setActiveChapter] = useState<string>('Capitolo 1');
+  const [selectedImages, setSelectedImages] = useState<{ [key: string]: Set<string> }>({ 'Capitolo 1': new Set() });
+
+  const addChapter = () => {
+    const newChapterNumber = Object.keys(chapters).length + 1;
+    const newChapter = `Capitolo ${newChapterNumber}`;
+    setChapters(prevChapters => ({ ...prevChapters, [newChapter]: [] }));
+    setSelectedImages(prevSelectedImages => ({ ...prevSelectedImages, [newChapter]: new Set() }));
+    setActiveChapter(newChapter);
+  };
+
+  const handleImageClick = useCallback((image: string) => {
+    setSelectedImages(prevSelectedImages => {
+      const newSelectedImages = { ...prevSelectedImages };
+      const chapterImages = newSelectedImages[activeChapter] || new Set();
+
+      if (chapterImages.has(image)) {
+        chapterImages.delete(image);
+      } else {
+        chapterImages.add(image);
+      }
+
+      newSelectedImages[activeChapter] = chapterImages;
+
+      // Update the chapters state
+      setChapters(prevChapters => ({
+        ...prevChapters,
+        [activeChapter]: Array.from(chapterImages)
+      }));
+
+      return newSelectedImages;
+    });
+  }, [activeChapter]);
+  
+  console.log(chapters)
+  console.log(selectedImages)
+  return (
+    <div className={`fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ${isOpen ? '' : 'hidden'}`}>
+      <div className="bg-white rounded-lg p-6 w-full max-w-5xl h-4/5 mx-auto overflow-x-auto">
+        <h2 className="text-2xl font-bold mb-4">Immagini estratte</h2>
+        <div className="flex items-center mb-4">
+          {Object.keys(chapters).map((chapter, index) => (
+            <button
+              key={index}
+              onClick={() => setActiveChapter(chapter)}
+              className={`px-4 py-2 rounded-t-lg ${activeChapter === chapter ? 'bg-gray-300' : 'bg-gray-200'} hover:bg-gray-300`}
+            >
+              {chapter}
+            </button>
+          ))}
+          <button onClick={addChapter} className="ml-4 text-blue-500 flex items-center">
+            <MdAddCircleOutline size={24} className="mr-1" /> Aggiungi Capitolo
+          </button>
+        </div>
+        <div className="flex space-x-4 overflow-x-scroll">
+          {images.map((image, index) => (
+            <div 
+            className={`image-container border border-gray-300 rounded-md p-2 ${selectedImages[activeChapter]?.has(image) ? 'border-blue-500' : ''}`}
+            key={`page_${index + 1}`}
+            onClick={() => handleImageClick(image)}
+            >
+              <img src={image} alt={`Page ${index + 1}`} className="max-w-none h-auto w-96" />
+            </div>
+          ))}
+        </div>
+        <div className="mt-4 flex justify-end">
+          <button
+            onClick={onClose}
+            className="bg-orange-500 text-white rounded-lg px-4 py-2 flex items-center"
+          >
+            Chiudi
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const FilePopup: React.FC<FilePopupProps> = ({ onClose, isOpen }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [numPages, setNumPages] = useState<number | null>(null);
+  const [imageUrlArray, setImageUrlArray] = useState<string[]>([]);
+  const [fileType, setFileType] = useState<string | null>(null);
+  const [selectedPDFFile, setSelectedPDFFile] = useState<File | null>(null);
+  const [isImagePopupOpen, setIsImagePopupOpen] = useState(false);
+
+  const handleImage = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setImageUrlArray([]);
+      const file = event.target.files?.[0];
+
+      if (file && file.type === 'application/pdf') {
+        setIsLoading(true);
+        setSelectedPDFFile(file);
+      } else if (file) {
+        setFileType('image');
+        setImageUrlArray([URL.createObjectURL(file)]);
+      }
+    },
+    [
+      setSelectedPDFFile,
+      setImageUrlArray,
+      imageUrlArray,
+      setIsLoading,
+      isLoading,
+    ]
+  );
+
+  const onLoadSuccess = useCallback(
+    ({ numPages }: { numPages: number }) => {
+      setNumPages(numPages);
+      setIsLoading(false);
+    },
+    [setNumPages, numPages, setIsLoading]
+  );
+
+  const renderPageToImage = async (pdf: any, pageNumber: number) => {
+    const page = await pdf.getPage(pageNumber);
+    const viewport = page.getViewport({ scale: 2.0 });
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderContext = {
+      canvasContext: context!,
+      viewport: viewport,
+    };
+
+    await page.render(renderContext).promise;
+
+    return new Promise<string>((resolve) => {
+      canvas.toBlob((blob) => {
+        if (blob) {
+          const imageUrl = URL.createObjectURL(blob);
+          resolve(imageUrl);
+        }
+      });
+    });
+  };
+
+  const processPdf = useCallback(async (file: File) => {
+    const arrayBuffer = await file.arrayBuffer();
+    const pdf = await pdfjs.getDocument({ data: arrayBuffer }).promise;
+    const imageUrls: string[] = [];
+
+    for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+      const imageUrl = await renderPageToImage(pdf, pageNum);
+      imageUrls.push(imageUrl);
+    }
+
+    setImageUrlArray(imageUrls);
+    setIsLoading(false);
+    setIsImagePopupOpen(true);
+  }, []);
+
+  React.useEffect(() => {
+    if (selectedPDFFile) {
+      processPdf(selectedPDFFile);
+    }
+  }, [selectedPDFFile, processPdf]);
+
+  console.log(numPages)
+  console.log(imageUrlArray)
+
+  return (
+    <div className={`fixed inset-0 z-50 flex w-100 items-center justify-center bg-black bg-opacity-50 ${isOpen ? '' : 'hidden'}`}>
+      <div className="bg-white rounded-lg p-6 w-full max-w-3xl mx-auto">
+        <h2 className="text-2xl font-bold mb-4">Card da Pdf</h2>
+        <div className="relative">
+          <input
+            type="file"
+            onChange={handleImage}
+            accept=".pdf"
+            className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+          />
+          <div className="bg-white border border-gray-200 rounded-lg p-4 flex flex-col items-center space-x-2 shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer">
+            <MdFileUpload size={24} className='mb-2' />
+            <span className="text-gray-700 font-medium">Carica File</span>
+          </div>
+        </div>
+        <div className="mt-4 flex justify-between items-center">
+          <button
+            onClick={onClose}
+            className="bg-white text-orange-500 border border-orange-500 rounded-lg px-4 py-2 hover:bg-orange-500 hover:text-white transition"
+          >
+            Chiudi
+          </button>
+          <button
+            onClick={() => console.log('Creazione card')}
+            className="bg-orange-500 text-white rounded-lg px-4 py-2 flex items-center"
+          >
+            Crea <span className="ml-2 text-yellow-300">🪙 15</span>
+          </button>
+        </div>
+        <div className="mt-4 text-sm text-gray-600">
+          oppure{' '}
+          <a href="#" className="underline">
+            carica un file dal tuo dispositivo
+          </a>
+        </div>
+        {isLoading && <div className="mt-4">Caricamento...</div>}
+
+        {/*selectedPDFFile && (
+          <div className="imageContainer">
+            <Document
+              file={selectedPDFFile}
+              onLoadSuccess={onLoadSuccess}
+              error={<div>An error occurred!</div>}
+            >
+              {Array.from(new Array(numPages || 0), (_, index) => (
+                <React.Fragment key={`page_${index + 1}`}>
+                  <Page
+                    pageNumber={index + 1}
+                    className={`import-pdf-page-${index + 1} image`}
+                    onRenderSuccess={() => onRenderSuccess(index)}
+                    width={1024}
+                    renderTextLayer={false}
+                    renderAnnotationLayer={false}
+                    error={<div>An error occurred!</div>}
+                  />
+                  {imageUrlArray[index] && (
+                    <a className='download' href={imageUrlArray[index]} download>
+                      download file
+                    </a>
+                  )}
+                </React.Fragment>
+              ))}
+            </Document>
+          </div>
+        )*/}
+
+        <ImagePopup isOpen={isImagePopupOpen} images={imageUrlArray} onClose={() => setIsImagePopupOpen(false)} />
+      </div>
+    </div>
+  );
+};
+
+
+export default FilePopup
