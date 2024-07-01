5dcc4bbfeafc2826f76cc0462a2a330c2f2d739e
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2024-07-01 23:03:14 +0200

    ook

--- client/app/(app)/map/head.tsx
+++ client/app/(app)/map/head.tsx
@@ -1,13 +1,13 @@
 export default function Head() {
 	return (
 		<>
-			<title>StudyAI | Ti aiutiamo nello studio</title>
+			<title>Studdy | High Quality Conceptual Maps for Quick Learning</title>
 			<meta content='width=device-width, initial-scale=1' name='viewport' />
 			<meta
 				name='description'
 				content='Crea facilente mappe concettuali partendo da documenti o immagini di testo.'
 			/>
-			<link rel='icon' href='/favicon.ico' />
+			<link rel='icon' href='/imgs/favicon.png' />
 		</>
 	);
 }
